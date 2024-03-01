709c331fe56d9be936af465da393899cdd9e987e
POLA <baltazarpilling10@gmail.com>
2024-03-01 17:16:25 -0300

    Read operation complete

--- main.cpp
+++ main.cpp
@@ -1,9 +1,126 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
+
+const string FILE_END = "" + (char)EOF;
+
+class File{
+    fstream self;
+    size_t pos = 1;
+    string path;
+
+    // Read a line and returns it (cycles).
+    string read(){
+        if(pos > this->size()){
+            pos = 1;
+            return FILE_END;
+        }
+        self.open(this->path.c_str(), ios::in);
+        if(!self.is_open())
+            return "";
+        
+        string line;
+        for(size_t i = 0; i < this->pos; i++)
+            getline(self, line);
+        pos += 1;
+
+
+        self.close();
+        return line;
+    }
+
+public:
+    
+    File(string path): path(path){}
+
+    size_t size(){
+        self.open(this->path.c_str(), ios::in);
+        if(!self.is_open())
+            return 0;
+        string line;
+        size_t i = 0;
+        while(getline(self, line)) i++;
+        
+        self.close();
+        return i;
+    }
+
+    /* Returns the extention of the file (text after the dot).
+    example.txt -> txt*/
+    string extention(){
+        int ext_index = this->path.rfind('.');
+        if(ext_index != string::npos){
+            string ext;
+            for(size_t i = ext_index + 1; i < path.size(); i++)
+                ext += path[i];
+            return ext;
+        }
+        return "";
+    }
+
+    /* Returns the name of the file.
+    ex/samples.txt -> samples*/
+    string name(){
+        int slash_i = this->path.rfind("/");
+        int dot_i = this->path.rfind(".");
+        
+        int start = (slash_i != string::npos) ? slash_i + 1 : 0;
+        int end = (dot_i != string::npos) ? dot_i : path.size();
+        string name;
+        for(size_t i = start; i < end; i++)
+            name += path[i];
+        return name;
+    }
+
+    // Restart the position of the line.
+    void restart(){
+        this->pos = 1;
+    }
+
+    // Reads a line in a buffer (returns FILE_END at the end).
+    bool read(string& buffer){
+        string line = this->read();
+        if(line != FILE_END){
+            buffer = line;
+            return true;
+        }
+        return false;
+    }
+
+    // Push back all the file lines in a vector.
+    void read(vector<string>& lines){
+        for(string line; this->read(line);)
+            lines.push_back(line);
+    }
+
+    // Reads a line and concatenates it to a string.
+    friend string& operator>>(File& input, string& output){
+        string line;
+        input.read(line);
+        output += line;
+        return output;
+    }
+
+    friend vector<string>& operator>>(File& input, vector<string>& output){
+        string line;
+        input.read(line);
+        output.push_back(line);
+        return output;
+    }
+};
+
+#define print(vect) for(size_t i = 0; i < vect.size(); i++){cout << vect[i] << '\n';}
 
 int main(int argc, char* argv[]){
     
-    cout << "Hello File\n";
+    File data("data.txt");
     
+    vector<string> lines;
+
+    data.read(lines);
+
+    print(lines);
+
     return 0;
 }
